086d0d768fb975c1c3645bddf3f0129df958d779
MohaProgra <58303651+MohaProgra@users.noreply.github.com>
2020-03-29 14:04:20 +0200

    Agregar archivos de proyecto.

---
+++ PokedexC_sharp/VentanaPrincipal.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PokedexC_sharp
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.izq = new System.Windows.Forms.Button();
+            this.der = new System.Windows.Forms.Button();
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 271);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // izq
+            // 
+            this.izq.Location = new System.Drawing.Point(13, 301);
+            this.izq.Name = "izq";
+            this.izq.Size = new System.Drawing.Size(75, 52);
+            this.izq.TabIndex = 1;
+            this.izq.Text = ">";
+            this.izq.UseVisualStyleBackColor = true;
+            this.izq.Click += new System.EventHandler(this.izq_Click);
+            // 
+            // der
+            // 
+            this.der.Location = new System.Drawing.Point(126, 301);
+            this.der.Name = "der";
+            this.der.Size = new System.Drawing.Size(75, 52);
+            this.der.TabIndex = 2;
+            this.der.Text = "<";
+            this.der.UseVisualStyleBackColor = true;
+            this.der.Click += new System.EventHandler(this.der_Click);
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.Location = new System.Drawing.Point(241, 13);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(184, 77);
+            this.nombrePokemon.TabIndex = 3;
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 459);
+            this.Controls.Add(this.nombrePokemon);
+            this.Controls.Add(this.der);
+            this.Controls.Add(this.izq);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "VentanaPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button izq;
+        private System.Windows.Forms.Button der;
+        private System.Windows.Forms.Label nombrePokemon;
+    }
+}
+
---
+++ PokedexC_sharp/VentanaPrincipal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokedexC_sharp
+{
+    public partial class VentanaPrincipal : Form
+    {
+
+        Conexion miConexion = new Conexion();
+        DataTable misPokemons = new DataTable();
+        int idActual = 1; //el pokemon que estamos viendo
+        
+
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void izq_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            if (idActual <= 0) { idActual = 1; }
+
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+        }
+
+        private void der_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            if (idActual <= 151) { idActual = 151; }
+
+            misPokemons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokemons.Rows[0]["nombre"].ToString();
+        }
+    }
+}
